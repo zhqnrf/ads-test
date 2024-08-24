8628a13096f437be46c4255ee183e070f080ac0f
Zhaqian Rouf Alfauzi <zhaqianroufa@gmail.com>
2024-08-24 23:14:04 +0700

    [add] add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,11 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Origins;
+use App\Models\Departures;
+use App\Models\Travel;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,10 +17,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Seed Users
+        $user1 = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin1234'),
+            'role' => 'admin',
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $user2 = User::create([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('user1234'),
+            'role' => 'user',
         ]);
 
         $origins = [
@@ -31,5 +44,35 @@ class DatabaseSeeder extends Seeder
                 'origin_name' => $origin,
             ]);
         }
+
+        $departures = [
+            '10:00',
+            '09:00',
+            '08:00',
+        ];
+
+        foreach ($departures as $departure) {
+            Departures::create([
+                'departure_category' => $departure,
+            ]);
+        }
+
+        $travel1 = Travel::create([
+            'travel_name' => 'Trip to Bali',
+            'travel_price' => 500,
+            'travel_picture' => 'storage/travel_pictures/medan.jpeg',
+            'id_origin' => 1,
+            'id_destination' => 2,
+            'id_departure' => 1,
+        ]);
+
+        $travel2 = Travel::create([
+            'travel_name' => 'Trip to Paris',
+            'travel_price' => 1500,
+            'travel_picture' => 'storage/travel_pictures/jakarta.jpeg',
+            'id_origin' => 2,
+            'id_destination' => 3,
+            'id_departure' => 2,
+        ]);
     }
 }
